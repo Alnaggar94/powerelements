5bac4b4f9f3b2589e80fd2c684d153b8568ae3b0
Chinmoy Paul <phpwebdeveloper29@gmail.com>
2023-07-25 11:01:45 +0530

    Add files via upload

---
+++ elements/element.php
@@ -0,0 +1,582 @@
+<?php
+
+namespace Upadans;
+
+use function Breakdance\Elements\c;
+use function Breakdance\Elements\PresetSections\getPresetSection;
+
+
+\Breakdance\ElementStudio\registerElementForEditing(
+    "Upadans\\CouponForm",
+    \Breakdance\Util\getdirectoryPathRelativeToPluginFolder(__DIR__)
+);
+
+class CouponForm extends \Breakdance\Elements\Element
+{
+    static function uiIcon()
+    {
+        return '<svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 384 512"><!--! Font Awesome Free 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M374.6 118.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-320 320c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l320-320zM128 128A64 64 0 1 0 0 128a64 64 0 1 0 128 0zM384 384a64 64 0 1 0 -128 0 64 64 0 1 0 128 0z"/></svg>';
+    }
+
+    static function tag()
+    {
+        return 'div';
+    }
+
+    static function tagOptions()
+    {
+        return [];
+    }
+
+    static function tagControlPath()
+    {
+        return false;
+    }
+
+    static function name()
+    {
+        return 'Coupon Form';
+    }
+
+    static function className()
+    {
+        return 'ue-coupon-form';
+    }
+
+    static function category()
+    {
+        return 'uwoo';
+    }
+
+    static function badge()
+    {
+        return ['label' => 'U', 'textColor' => 'var(--white)', 'backgroundColor' => 'var(--yellow500)'];
+    }
+
+    static function slug()
+    {
+        return get_class();
+    }
+
+    static function template()
+    {
+        return file_get_contents(__DIR__ . '/html.twig');
+    }
+
+    static function defaultCss()
+    {
+        return file_get_contents(__DIR__ . '/default.css');
+    }
+
+    static function defaultProperties()
+    {
+        return ['content'=>['layout'=>['hascouponform'=>true]]];
+    }
+
+    static function defaultChildren()
+    {
+        return false;
+    }
+
+    static function cssTemplate()
+    {
+        $template = file_get_contents(__DIR__ . '/css.twig');
+        return $template;
+    }
+
+    static function designControls()
+    {
+        return [];
+    }
+
+    static function contentControls()
+    {
+        return [c(
+        "layout",
+        "Layout",
+        [c(
+        "grid_template_columns",
+        "Grid template columns",
+        [],
+        ['type' => 'text', 'layout' => 'vertical', 'placeholder' => '65% 1fr'],
+        false,
+        false,
+        [],
+      ), c(
+        "gap",
+        "Gap",
+        [],
+        ['type' => 'unit', 'layout' => 'inline', 'unitOptions' => ['types' => ['0' => 'px', '1' => 'em', '2' => 'rem', '3' => '%', '4' => 'vh', '5' => 'vw', '6' => 'calc', '7' => 'auto', '8' => 'custom'], 'defaultType' => 'px']],
+        false,
+        false,
+        [],
+      ), c(
+        "hascouponform",
+        "hasCouponForm",
+        [],
+        ['type' => 'toggle', 'layout' => 'inline', 'hideForElements' => ['0' => 'Upadans\CouponForm']],
+        false,
+        false,
+        [],
+      )],
+        ['type' => 'section', 'layout' => 'vertical'],
+        false,
+        false,
+        [],
+      ), c(
+        "input_field",
+        "Input Field",
+        [c(
+        "label",
+        "Label / Placeholder",
+        [],
+        ['type' => 'text', 'layout' => 'vertical', 'placeholder' => 'Enter coupon code'],
+        false,
+        false,
+        [],
+      ), c(
+        "order",
+        "Order",
+        [],
+        ['type' => 'number', 'layout' => 'inline', 'rangeOptions' => ['min' => 0, 'max' => 5, 'step' => 1]],
+        false,
+        false,
+        [],
+      ), c(
+        "width",
+        "Width",
+        [],
+        ['type' => 'unit', 'layout' => 'inline', 'unitOptions' => ['types' => ['0' => 'px', '1' => 'em', '2' => '%', '3' => 'vw', '4' => 'calc', '5' => 'auto', '6' => 'custom'], 'defaultType' => 'px']],
+        false,
+        false,
+        [],
+      ), c(
+        "height",
+        "Height",
+        [],
+        ['type' => 'unit', 'layout' => 'inline', 'unitOptions' => ['types' => ['0' => 'px', '1' => 'em', '2' => '%', '3' => 'vh', '4' => 'calc', '5' => 'auto', '6' => 'custom'], 'defaultType' => 'px']],
+        false,
+        false,
+        [],
+      ), c(
+        "background",
+        "Background",
+        [],
+        ['type' => 'color', 'layout' => 'inline'],
+        false,
+        false,
+        [],
+      ), c(
+        "placeholder",
+        "Placeholder",
+        [],
+        ['type' => 'color', 'layout' => 'inline'],
+        false,
+        false,
+        [],
+      ), getPresetSection(
+      "EssentialElements\\spacing_padding_all",
+      "Padding",
+      "padding",
+       ['type' => 'popout']
+     ), getPresetSection(
+      "EssentialElements\\typography",
+      "Typography",
+      "typography",
+       ['type' => 'popout']
+     ), getPresetSection(
+      "EssentialElements\\borders",
+      "Borders",
+      "borders",
+       ['type' => 'popout']
+     ), c(
+        "focus",
+        "Focus",
+        [],
+        ['type' => 'alert_box', 'layout' => 'vertical', 'alertBoxOptions' => ['style' => 'default', 'content' => '<p>Focus Style</p>']],
+        false,
+        false,
+        [],
+      ), c(
+        "focus_background",
+        "Background",
+        [],
+        ['type' => 'color', 'layout' => 'inline'],
+        false,
+        false,
+        [],
+      ), c(
+        "focus_color",
+        "Color",
+        [],
+        ['type' => 'color', 'layout' => 'inline'],
+        false,
+        false,
+        [],
+      ), getPresetSection(
+      "EssentialElements\\borders",
+      "Borders",
+      "focus_borders",
+       ['type' => 'popout']
+     )],
+        ['type' => 'section', 'layout' => 'vertical'],
+        false,
+        false,
+        [],
+      ), c(
+        "apply_button",
+        "Apply Button",
+        [c(
+        "button_text",
+        "Button text",
+        [],
+        ['type' => 'text', 'layout' => 'vertical', 'placeholder' => 'Apply'],
+        false,
+        false,
+        [],
+      ), c(
+        "order",
+        "Order",
+        [],
+        ['type' => 'number', 'layout' => 'inline', 'rangeOptions' => ['min' => 0, 'max' => 5, 'step' => 1]],
+        false,
+        false,
+        [],
+      ), c(
+        "width",
+        "Width",
+        [],
+        ['type' => 'unit', 'layout' => 'inline', 'unitOptions' => ['types' => ['0' => 'px', '1' => 'em', '2' => '%', '3' => 'vw', '4' => 'calc', '5' => 'auto', '6' => 'custom'], 'defaultType' => 'px']],
+        false,
+        false,
+        [],
+      ), c(
+        "height",
+        "Height",
+        [],
+        ['type' => 'unit', 'layout' => 'inline', 'unitOptions' => ['types' => ['0' => 'px', '1' => '%', '2' => 'vh', '3' => 'calc', '4' => 'auto', '5' => 'custom'], 'defaultType' => 'px']],
+        false,
+        false,
+        [],
+      ), c(
+        "background",
+        "Background",
+        [],
+        ['type' => 'color', 'layout' => 'inline'],
+        false,
+        false,
+        [],
+      ), getPresetSection(
+      "EssentialElements\\spacing_all",
+      "Spacing",
+      "spacing",
+       ['type' => 'popout']
+     ), getPresetSection(
+      "EssentialElements\\typography",
+      "Typography",
+      "typography",
+       ['type' => 'popout']
+     ), getPresetSection(
+      "EssentialElements\\borders",
+      "Borders",
+      "borders",
+       ['type' => 'popout']
+     ), c(
+        "hover",
+        "Hover",
+        [],
+        ['type' => 'alert_box', 'layout' => 'vertical', 'alertBoxOptions' => ['style' => 'default', 'content' => '<p>Hover style</p>']],
+        false,
+        false,
+        [],
+      ), c(
+        "hover_background",
+        "Background",
+        [],
+        ['type' => 'color', 'layout' => 'inline'],
+        false,
+        false,
+        [],
+      ), c(
+        "hover_color",
+        "Color",
+        [],
+        ['type' => 'color', 'layout' => 'inline'],
+        false,
+        false,
+        [],
+      ), getPresetSection(
+      "EssentialElements\\borders",
+      "Borders",
+      "hover_borders",
+       ['type' => 'popout']
+     )],
+        ['type' => 'section', 'layout' => 'vertical'],
+        false,
+        false,
+        [],
+      ), c(
+        "success_message",
+        "Success Message",
+        [c(
+        "preview",
+        "Preview In-Builder",
+        [],
+        ['type' => 'toggle', 'layout' => 'inline'],
+        false,
+        false,
+        [],
+      ), c(
+        "hide",
+        "Hide the message",
+        [],
+        ['type' => 'toggle', 'layout' => 'inline'],
+        false,
+        false,
+        [],
+      ), c(
+        "background",
+        "Background",
+        [],
+        ['type' => 'color', 'layout' => 'inline'],
+        false,
+        false,
+        [],
+      ), getPresetSection(
+      "EssentialElements\\spacing_padding_all",
+      "Padding",
+      "padding",
+       ['type' => 'popout']
+     ), getPresetSection(
+      "EssentialElements\\typography",
+      "Typography",
+      "typography",
+       ['type' => 'popout']
+     ), getPresetSection(
+      "EssentialElements\\borders",
+      "Borders",
+      "borders",
+       ['type' => 'popout']
+     ), c(
+        "order",
+        "Order",
+        [],
+        ['type' => 'number', 'layout' => 'inline', 'rangeOptions' => ['min' => 0, 'max' => 5, 'step' => 1]],
+        false,
+        false,
+        [],
+      )],
+        ['type' => 'section', 'layout' => 'vertical'],
+        false,
+        false,
+        [],
+      ), c(
+        "error_message",
+        "Error Message",
+        [c(
+        "preview",
+        "Preview In-Builder",
+        [],
+        ['type' => 'toggle', 'layout' => 'inline'],
+        false,
+        false,
+        [],
+      ), c(
+        "hide",
+        "Hide the message",
+        [],
+        ['type' => 'toggle', 'layout' => 'inline'],
+        false,
+        false,
+        [],
+      ), c(
+        "background",
+        "Background",
+        [],
+        ['type' => 'color', 'layout' => 'inline'],
+        false,
+        false,
+        [],
+      ), getPresetSection(
+      "EssentialElements\\spacing_padding_all",
+      "Padding",
+      "padding",
+       ['type' => 'popout']
+     ), getPresetSection(
+      "EssentialElements\\typography",
+      "Typography",
+      "typography",
+       ['type' => 'popout']
+     ), getPresetSection(
+      "EssentialElements\\borders",
+      "Borders",
+      "borders",
+       ['type' => 'popout']
+     ), c(
+        "order",
+        "Order",
+        [],
+        ['type' => 'number', 'layout' => 'inline', 'rangeOptions' => ['min' => 0, 'max' => 5, 'step' => 1]],
+        false,
+        false,
+        [],
+      )],
+        ['type' => 'section', 'layout' => 'vertical'],
+        false,
+        false,
+        [],
+      ), c(
+        "label_animation",
+        "Label Animation",
+        [c(
+        "enable_animation",
+        "Enable animation",
+        [],
+        ['type' => 'toggle', 'layout' => 'inline'],
+        false,
+        false,
+        [],
+      ), getPresetSection(
+      "EssentialElements\\typography",
+      "Typography",
+      "typography",
+       ['type' => 'popout']
+     ), c(
+        "transition",
+        "Transition duration",
+        [],
+        ['type' => 'unit', 'layout' => 'inline', 'rangeOptions' => ['min' => 0, 'max' => 20, 'step' => 0.1], 'unitOptions' => ['types' => ['0' => 's'], 'defaultType' => 's']],
+        false,
+        false,
+        [],
+      ), c(
+        "position_left",
+        "Position: Left",
+        [],
+        ['type' => 'unit', 'layout' => 'inline', 'rangeOptions' => ['min' => 0, 'max' => 100, 'step' => 1]],
+        false,
+        false,
+        [],
+      ), c(
+        "after_animation",
+        "After animation",
+        [],
+        ['type' => 'alert_box', 'layout' => 'vertical', 'alertBoxOptions' => ['style' => 'default', 'content' => '<p>After animation</p>']],
+        false,
+        false,
+        [],
+      ), c(
+        "font_size",
+        "Font size",
+        [],
+        ['type' => 'unit', 'layout' => 'inline'],
+        false,
+        false,
+        [],
+      ), c(
+        "background",
+        "Background",
+        [],
+        ['type' => 'color', 'layout' => 'inline'],
+        false,
+        false,
+        [],
+      ), c(
+        "label_color",
+        "Label color",
+        [],
+        ['type' => 'color', 'layout' => 'inline'],
+        false,
+        false,
+        [],
+      ), c(
+        "after_position_left",
+        "Position: Left",
+        [],
+        ['type' => 'unit', 'layout' => 'inline'],
+        false,
+        false,
+        [],
+      )],
+        ['type' => 'section', 'layout' => 'vertical'],
+        false,
+        false,
+        [],
+      )];
+    }
+
+    static function settingsControls()
+    {
+        return [];
+    }
+
+    static function dependencies()
+    {
+        return false;
+    }
+
+    static function settings()
+    {
+        return ['requiredPlugins' => ['0' => 'WooCommerce']];
+    }
+
+    static function addPanelRules()
+    {
+        return false;
+    }
+
+    static public function actions()
+    {
+        return false;
+    }
+
+    static function nestingRule()
+    {
+        return ["type" => "final",   ];
+    }
+
+    static function spacingBars()
+    {
+        return false;
+    }
+
+    static function attributes()
+    {
+        return false;
+    }
+
+    static function experimental()
+    {
+        return false;
+    }
+
+    static function order()
+    {
+        return 30;
+    }
+
+    static function dynamicPropertyPaths()
+    {
+        return ['0' => ['path' => 'settings.advanced.attributes[].value', 'accepts' => 'string'], '1' => ['path' => 'settings.advanced.attributes[].value', 'accepts' => 'string'], '2' => ['path' => 'settings.advanced.attributes[].value', 'accepts' => 'string'], '3' => ['path' => 'settings.advanced.attributes[].value', 'accepts' => 'string']];
+    }
+
+    static function additionalClasses()
+    {
+        return false;
+    }
+
+    static function projectManagement()
+    {
+        return false;
+    }
+
+    static function propertyPathsToWhitelistInFlatProps()
+    {
+        return false;
+    }
+
+    static function propertyPathsToSsrElementWhenValueChanges()
+    {
+        return false;
+    }
+}
